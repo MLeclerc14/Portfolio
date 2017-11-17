8676c8c325f669ba3b10d924c1107d67c2cfa6fb
Maxime LECLERC <contact@maxime-leclerc.com>
2017-11-17 15:50:32 +0100

    feat(api): add api home

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,20 @@
  import { Component } from '@angular/core';
+ import { HttpClient } from '@angular/http';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit  {
+
+  results: string[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://server.dev/api/home').subscribe(res => {
+      console.log(res);
+      this.results = res.data;
+    });
+  }
+}
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
-  imports: [MaterialModule],
+  imports: [MaterialModule, HttpClientModule,],
   declarations: [HomeComponent]
 })
 export class HomeModule { }
